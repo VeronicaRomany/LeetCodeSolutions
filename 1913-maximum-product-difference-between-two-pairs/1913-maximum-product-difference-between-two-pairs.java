f97173fac87feae11e0c9131a46354553527da6d
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2023-12-18 02:42:00 +0200

    Time: 2 ms (97.51%), Space: 45.1 MB (6.62%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.java
@@ -2,20 +2,43 @@ class Solution {
     public int maxProductDifference(int[] nums) {
      
         
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        PriorityQueue<Integer> pqReversed = new PriorityQueue<>(Collections.reverseOrder());
+       int max = 0;
+       int min = Integer.MAX_VALUE;
         
-        
+        int k=0;
+        int j=0;
         
         for(int i=0 ; i<nums.length ; i++){
-            pq.add(nums[i]);
-            pqReversed.add(nums[i]);
+            if(nums[i]>max){
+                max = nums[i];
+                k=i;
+            }
+            
+            if(nums[i] < min){
+                min = nums[i];
+                j=i;
+            }
         }
         
-       
+        nums[k]= 0;
+        nums[j]= 0;
         
+        int smax = 0;
+        int smin = Integer.MAX_VALUE;
         
-        return pqReversed.poll() * pqReversed.poll() - pq.poll() * pq.poll();
+         for(int i=0 ; i<nums.length ; i++){
+            if(nums[i] > smax){
+                smax = nums[i];
+               
+            }
+            
+            if(nums[i] < smin && nums[i] > 0){
+                smin = nums[i];
+              
+            }
+        }
+        
+        return max*smax - min*smin;
         
     }
 }
