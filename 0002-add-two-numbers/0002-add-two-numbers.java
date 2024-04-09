8db3ac712b397d9434c9b2b0881534a35815cdb9
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2024-04-09 16:31:37 +0200

    Time: 1 ms (100.00%), Space: 44.2 MB (65.86%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -16,80 +16,26 @@ class Solution {
         output = ans;
         int reminder = 0;
         
-        while(l1 != null && l2 != null){
-            int sum = reminder + l1.val + l2.val;
-            int newCellVal = sum % 10;
+        while(l1 != null || l2 != null || reminder!=0){
+            int sum = reminder + (l1 !=null ? l1.val : 0 ) + (l2 != null ? l2.val : 0);
+            int value = sum % 10;
             reminder = sum / 10;
             
-            System.out.println("val " + newCellVal + " rem " + reminder);
+            ans.val = value;
             
-            ans.val = newCellVal;
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
             
-            // System.out.println("in output" + ans.val);
-          
-            
-            l1 = l1.next;
-            l2 = l2.next;
-            
-            if(l1 !=null && l2 !=null){
-                  ans.next = new ListNode();
-                  ans = ans.next;
+      
+            if (l1 != null || l2 != null || reminder != 0) {
+                ans.next = new ListNode(); 
+                ans = ans.next; 
             }
-            
         }
         
-        if(l1 !=null || l2 !=null){
-            ans.next = new ListNode();
-            ans = ans.next;
+        if(reminder != 0){
+            ans.val = reminder;
         }
-        
-        while(l1 != null){
-            // System.out.println("in l1 not empty");
-            int sum = reminder + l1.val;
-            int newCellVal = sum % 10;
-            reminder = sum / 10;
-            
-            ans.val = newCellVal;
-       
-            
-            l1 = l1.next;
-            
-            if(l1 !=null){
-                  ans.next = new ListNode();
-                  ans = ans.next;
-            }
-          
-            
-        }
-        
-        while(l2 !=null){
-             // System.out.println("in l2 not empty");
-            int sum = reminder + l2.val;
-            int newCellVal = sum % 10;
-            reminder = sum / 10;
-            
-            ans.val = newCellVal;
-            
-            
-            l2 = l2.next;
-            
-            
-            if(l2 !=null){
-                ans.next = new ListNode();
-                ans = ans.next;
-            }
-            
-        }
-        
-        if(reminder !=0){
-            ans.next = new ListNode();
-            ans = ans.next;
-            // System.out.println("last reminder is = " + reminder);
-            ans.val= reminder;
-          
-        }
-        
-
         
         return output;
     }
