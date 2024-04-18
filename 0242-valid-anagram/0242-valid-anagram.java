68505e670b7b2d3ef94520f781bf577454343ecb
VeronicaRomany <77462942+VeronicaRomany@users.noreply.github.com>
2024-04-18 23:18:53 +0200

    Time: 5 ms (53.93%), Space: 42.9 MB (83.09%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -3,15 +3,15 @@ class Solution {
         
         if(s.length() != t.length()) return false;
         
-        char[] S = s.toCharArray();
-        char[] T = t.toCharArray();
+//         char[] S = s.toCharArray();
+//         char[] T = t.toCharArray();
         
         int [] Freq = new int [26];
         // int [] tFreq = new int [26];
         
         for(int i=0 ; i<s.length() ; i++){
-            Freq[(int) (S[i]-'a')]++;
-            Freq[(int) (T[i]-'a')]--;
+            Freq[(s.charAt(i)-'a')]++;
+            Freq[(t.charAt(i)-'a')]--;
         }
         
         for(int i=0 ; i<26;i++){
